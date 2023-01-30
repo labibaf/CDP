e102e2fb0cf7518597e1b31f70278192fbf146a4
Labib Fadhali <32814972+labibaf@users.noreply.github.com>
2023-01-30 16:33:47 +0700

    Create test_kalman_loadcell.ino

---
+++ test_kalman_loadcell.ino
@@ -0,0 +1,52 @@
+#include "HX711.h"
+
+const int DOUT_PIN = 21;
+const int SCK_PIN = 19;
+const float CALIBRATION_FACTOR = 0.42;
+
+HX711 scale;
+
+int btn=23;
+int btnstate=0;
+
+// kalman
+float SensorData,KalmanFilterData;
+float Xt,Xt_update,Xt_prev;
+float Pt,Pt_update, Pt_prev;
+float Kt,R,Q;
+
+void setup() {
+  Serial.begin(115200);
+  scale.begin(DOUT_PIN, SCK_PIN);
+  scale.set_scale(CALIBRATION_FACTOR);
+  scale.tare();
+  R=100;Q=1;Pt_prev + Q;
+
+  pinMode(btn,INPUT);
+}
+
+void loop() {
+  btnstate=digitalRead(btn);
+  if(btnstate=HIGH){
+    scale.tare();
+  }
+  float weight = scale.get_units();
+  Serial.print(weight);
+
+  SensorData=weight;
+  Xt_update=Xt_prev;
+  Pt_update=Pt_prev+Q;
+
+  Kt=Pt_update/(Pt_update+R);
+  Xt=Xt_update+(Kt*(SensorData-Xt_update));
+  Pt=(1-Kt)*Pt_update;
+
+  Xt_prev=Xt;
+  Pt_prev=Pt;
+  
+  KalmanFilterData=Xt;
+  Serial.print(",");
+  Serial.print(KalmanFilterData);
+  Serial.println();
+  delayMicroseconds(100);
+}
